e9362c30dc18082683a3c783223f72b6409e170f
Pedro Manuel Jurado Moreno <mpedrojurado@gmail.com>
2021-03-13 20:43:13 -0500

    filtrar datos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { LocalData } from './dataLocal';
 import './App.css';
+import github from './assets/image/github.png';
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props) {
+      super(props)
+      this.state = {
+          // categoriaElegida: this.props.match.params.id ,
+          listaAviso: [], // arreglo para guardar la lista
+
+      }
+      console.log("CONSTRUCTOR")
+
+      //this.obtenerAvisoSolo = this.obtenerAvisoSolo.bind(this);
+  }
+
+  componentDidMount() {
+      this.setState({
+          listaAviso: LocalData, // lista == dataLocal
+      })
+      console.log(LocalData)
+  }
+
+  dibujarAviso(datosAviso) {
+
+      return (      
+      <>
+              {datosAviso.filter(itemAviso=>
+                  itemAviso.curso == 'Angular').map(filteredAviso => (
+                     <div className="row" key={filteredAviso.id}>    
+                          <div className="col">
+                              <div>
+                                <h4 id="tit-blog" >{filteredAviso.titulo}
+                                <span className=
+                                      {` 
+                                      ${filteredAviso .curso === "React" ? 'badge badge-pill badge-info float-right tit-bad' : ''}
+                                      ${filteredAviso .curso  === "Angular" ? 'badge badge-pill badge-danger float-right tit-bad' : ''}
+                                      `}
+                              >&nbsp;&nbsp;&nbsp;{filteredAviso .curso}&nbsp;&nbsp;&nbsp;</span>
+                                
+                                </h4> 
+                                <span className="text-muted float-right">&nbsp;&nbsp;<b>&nbsp;&nbsp;{filteredAviso.fecha}</b></span>
+                              </div>
+                              <img src={'../img/'+filteredAviso.foto} className="img-fluid rounded"  width="100%"/>
+                              <br></br><br></br>
+                              <p>
+                                  {filteredAviso .subtitulo}
+                              </p>   
+                          </div>
+                     </div>            
+                  ))}
+        </>
+      )
+  }
+
+  dibujarAviso2(datosAviso) {
+
+    return (      
+    <>
+     
+          <div class="input-group mb-3">
+            <select class="custom-select" id="inputGroupSelect02"  value={this.state.buscar}  onChange={(e) => this.setState({ buscar: e.target.value })}>
+              <option selected>Seleccione la opción</option>
+              <option value="Angular">Angular</option>
+              <option value="React">React</option>
+              <option value="Hacking">Hacking</option>
+
+            </select>
+            <div class="input-group-append">
+              <label class="input-group-text" for="inputGroupSelect02">Filtrar</label>
+            </div>
+          </div>
+            {datosAviso.filter(itemAviso=>
+                itemAviso.curso == this.state.buscar).map(filteredAviso => (
+                   <div className="row" key={filteredAviso.id}>    
+                        <div className="col">
+                            <div>
+                              <h4 id="tit-blog" >{filteredAviso.titulo}
+                              <span className=
+                                    {` 
+                                    ${filteredAviso .curso === "React" ? 'badge badge-pill badge-info float-right tit-bad' : ''}
+                                    ${filteredAviso .curso  === "Angular" ? 'badge badge-pill badge-danger float-right tit-bad' : ''}
+                                    `}
+                            >&nbsp;&nbsp;&nbsp;{filteredAviso .curso}&nbsp;&nbsp;&nbsp;</span>
+                              
+                              </h4> 
+                              <span className="text-muted float-right">&nbsp;&nbsp;<b>&nbsp;&nbsp;{filteredAviso.fecha}</b></span>
+                            </div>
+                            <img src={'../img/'+filteredAviso.foto} className="img-fluid rounded"  width="100%"/>
+                            <br></br><br></br>
+                            <p>
+                                {filteredAviso .subtitulo}
+                            </p>   
+                        </div>
+                   </div>            
+                ))}
+      </>
+    )
 }
+  render() {
+ 
+     let contenido =this.dibujarAviso(this.state.listaAviso);
+     let contenido2 =this.dibujarAviso2(this.state.listaAviso);
+      return (
+          <>
+            <div id="head" className="container-fluid">
+                <div className="row">
+                    <div  className="col-md-8">
+                      <h3 className="text-center text-white">FILTER CON .MAP</h3>
+                    </div>
+                    <div className="col-md-4">           
+                        <h5 className="float-right text-dark" style={{marginTop:"5px"}}>PedroManuelJM&nbsp;&nbsp;
+                                    <a  href="https://github.com/PedroManuelJM" target="_blank">
+                                      <img  src={github}  style={{marginRight:"20px"}}width="9%"/>
+                                    </a>
+                        </h5>        
+                    </div>
+                   
+                </div>
+             </div>
+             <div className="container" style={{marginTop:"20px"}}>
+                <div className="row">
+                    <div className="col-md-6">
+                       {contenido}
+                    </div>
+                    <div className="col-md-6">
+                       {contenido2}
+                    </div>
+                </div> 
+             </div>
 
-export default App;
+           
+          </>
+
+      );
+  }
+}
---
+++ src/dataLocal/index.js
@@ -0,0 +1,8 @@
+  
+export  const LocalData = [
+    {id: 1,titulo: "CREANDO PROYECTO EN REACT ",subtitulo:"Aprenderas como crear una aplicación en React y poder deplegarlo en github Pages o Firebase. ",curso:"React",foto:"react.jpg" ,fecha:"08-03-2021"},
+    {id: 2,titulo: "CREANDO PROYECTO EN ANGULAR",subtitulo:"Aprenderas como crear una aplicación en Angular y poder deplegarlo en github Pages",curso:"Angular",foto:"angular.jpg",fecha:"08-03-2021"},
+    {id: 3,titulo: "VARIAS CLASES DE CSS EN REACT",subtitulo:"Hay muchas técnicas de como realizar múltiples CSS , pero este ejemplo se mostrará de una sola manera. ",curso:"React",foto:"multipleclasescss.png" ,fecha:"10-03-2021"},
+    {id: 4,titulo: "ANGULAR COMPONENTS",subtitulo:"Navegación entre componentes en Angular 10.",curso:"Angular",foto:"angular-component.png" ,fecha:"10-03-2021"},
+    {id: 5,titulo: "ETHICAL HACKING BÁSICO",subtitulo:" Aprenderas reconocimiento, Escaneo ,Enumeración ,Análisis de vulnerabilidades ,Explotación, Reporte , Hacking de servicios de red, Hacking de sistemas operativos,Hacking de aplicaciones web.",curso:"Hacking",foto:"hacking.png" ,fecha:"13-03-2021"}
+]
